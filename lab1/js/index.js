d745e255882d8cb4e6f9017f61b592a9251906a6
Sofia Marcano <soamaia03@gmail.com>
2025-05-06 19:03:46 -0400

    listado de estudiantes dinámico usando javascript

--- lab1/js/index.js
+++ lab1/js/index.js
@@ -2,7 +2,7 @@ const logo = document.querySelector('#logo');
 logo.innerHTML = `${config.sitio[0]} <span>${config.sitio[1]}</span> ${config.sitio[2]}`;
 
 const mid = document.querySelector('#mid');
-mid.innerHTML = `${config.saludo}, Sofía`;
+mid.innerHTML = `${config.saludo}, ${perfiles[0].nombre}`;
 
 const searchBar = document.querySelector('li form');
 searchBar.querySelector('input').setAttribute('placeholder', `${config.nombre}...`);
@@ -12,3 +12,11 @@ const footer = document.querySelector('footer');
 footer.innerHTML = `${config.copyRight}`;
 
 const gallery = document.querySelector('#gallery');
+
+perfiles.forEach(perfil => {
+    const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
+    galleryItem.querySelector('a').setAttribute('href', `${perfil.ci}/perfil.html`)
+    galleryItem.querySelector('.galleryimg').setAttribute('src', `${perfil.imagen}`);
+    galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
+    gallery.appendChild(galleryItem);
+});
