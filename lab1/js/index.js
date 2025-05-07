ee1e893b62cba5c8d31a38ed25ca7022ca699312
Sofia Marcano <soamaia03@gmail.com>
2025-05-07 18:51:20 -0400

    búsqueda de estudiantes

--- lab1/js/index.js
+++ lab1/js/index.js
@@ -2,7 +2,11 @@ window.onload = function () {
     const language = new URLSearchParams(window.location.search).get("lang");
     if (!language) {
         let url = window.location.href;
-        url += "?lang=ES";
+        if (url.indexOf('?') > -1){
+            url += 'lang=ES'
+         } else {
+            url += '?lang=ES'
+         }
         window.location.href = url;
     }
 
@@ -27,13 +31,44 @@ window.onload = function () {
         
     document.body.appendChild(configLan);
 
-    const gallery = document.querySelector('#gallery');
+    function renderProfiles(perfiles) {
+        const gallery = document.querySelector('#gallery');
 
-    perfiles.forEach(perfil => {
-        const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
-        galleryItem.querySelector('a').setAttribute('href', `perfil.html?ci=${perfil.ci}&lang=${language}`)
-        galleryItem.querySelector('.galleryimg').setAttribute('src', `${perfil.imagen}`);
-        galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
-        gallery.appendChild(galleryItem);
+        perfiles.forEach(perfil => {
+            const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
+            galleryItem.querySelector('a').setAttribute('href', `perfil.html?ci=${perfil.ci}&lang=${language}`)
+            galleryItem.querySelector('.galleryimg').setAttribute('src', `${perfil.imagen}`);
+            galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
+            gallery.appendChild(galleryItem);
+        });
+    };
+
+    renderProfiles(perfiles);
+
+    const searchBar = document.getElementById('searchBar');
+    const searchButton = document.getElementById('searchButton');
+
+    searchButton.addEventListener('click', function(event) {
+        const query = searchBar.value.toLowerCase();
+        const results = perfiles.filter(perfil => perfil.nombre.toLowerCase().includes(query));
+
+        const section = document.querySelector('section');
+        section.innerHTML = `<ul id = "gallery"></ul>`;
+
+        if (query == "") {
+            renderProfiles(perfiles);
+        }else if (results.length > 0) {
+            renderProfiles(results);
+        } else {
+            const p = document.createElement('p');
+            p.id = "no_result";
+            p.innerHTML = config.no_resultado.replace('[query]', query);
+            document.querySelector('section').appendChild(p);
+        }
+        
+        event.preventDefault();
     });
+
+    
+    
 };
