35a627abbf5c44d05bf75aa7a3572a5eed6c9ef2
Sofia Marcano <soamaia03@gmail.com>
2025-05-07 18:03:04 -0400

    cambio de lenguaje según atributo

--- lab1/js/index.js
+++ lab1/js/index.js
@@ -1,22 +1,39 @@
-const logo = document.querySelector('#logo');
-logo.innerHTML = `${config.sitio[0]} <span>${config.sitio[1]}</span> ${config.sitio[2]}`;
+window.onload = function () {
+    const language = new URLSearchParams(window.location.search).get("lang");
+    if (!language) {
+        let url = window.location.href;
+        url += "?lang=ES";
+        window.location.href = url;
+    }
 
-const mid = document.querySelector('#mid');
-mid.innerHTML = `${config.saludo}, ${perfiles[0].nombre}`;
+    const configLan = document.createElement('script');
+    configLan.src = `conf/config${language}.json`;
+    configLan.onload = function () {
 
-const searchBar = document.querySelector('li form');
-searchBar.querySelector('input').setAttribute('placeholder', `${config.nombre}...`);
-searchBar.querySelector('button').innerHTML = `${config.buscar}`;
+        const logo = document.querySelector('#logo');
+        logo.innerHTML = `${config.sitio[0]} <span>${config.sitio[1]}</span> ${config.sitio[2]}`;
 
-const footer = document.querySelector('footer');
-footer.innerHTML = `${config.copyRight}`;
+        const mid = document.querySelector('#mid');
+        mid.innerHTML = `${config.saludo}, ${perfiles[0].nombre}`;
 
-const gallery = document.querySelector('#gallery');
+        const searchBar = document.querySelector('li form');
+        searchBar.querySelector('input').setAttribute('placeholder', `${config.nombre}...`);
+        searchBar.querySelector('button').innerHTML = `${config.buscar}`;
 
-perfiles.forEach(perfil => {
-    const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
-    galleryItem.querySelector('a').setAttribute('href', `perfil.html?ci=${perfil.ci}`)
-    galleryItem.querySelector('.galleryimg').setAttribute('src', `${perfil.imagen}`);
-    galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
-    gallery.appendChild(galleryItem);
-});
+        const footer = document.querySelector('footer');
+        footer.innerHTML = `${config.copyRight}`;
+
+    }
+        
+    document.body.appendChild(configLan);
+
+    const gallery = document.querySelector('#gallery');
+
+    perfiles.forEach(perfil => {
+        const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
+        galleryItem.querySelector('a').setAttribute('href', `perfil.html?ci=${perfil.ci}&lang=${language}`)
+        galleryItem.querySelector('.galleryimg').setAttribute('src', `${perfil.imagen}`);
+        galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
+        gallery.appendChild(galleryItem);
+    });
+};
