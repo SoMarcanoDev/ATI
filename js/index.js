e31b2d3c5aca522e7d5d6b0159d7c6c2d3066d82
Sofia Marcano <soamaia03@gmail.com>
2025-06-12 18:14:23 -0400

    Transformacion de MPA a SPA

--- js/index.js
+++ js/index.js
@@ -1,74 +1,84 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
+
     const language = new URLSearchParams(this.location.search).get("lang");
     if (!language) {
         let url = this.location.href;
+        console.log(this)
         if (url.indexOf('?') > -1){
             url += 'lang=ES'
-         } else {
+        } else {
             url += '?lang=ES'
-         }
+        }
         this.location.href = url;
     }
-
-    const configLan = document.createElement('script');
-    configLan.src = `conf/config${language}.json`;
-    configLan.onload = function () {
-
+    
+    const config = fetch(`conf/config${language}.json`).then(response => response.json());
+    const data = fetch('datos/index.json').then(response => response.json());
+    
+    Promise.all([config, data])
+    .then(([config, data]) => {
         const logo = document.querySelector('#logo');
         logo.innerHTML = `${config.sitio[0]} <span>${config.sitio[1]}</span> ${config.sitio[2]}`;
 
+        const searchForm = document.querySelector('li form');
+        searchForm.querySelector('input').setAttribute('placeholder', `${config.nombre}...`);
+        searchForm.querySelector('button').innerHTML = `${config.buscar}`;
+        
         const mid = document.querySelector('#mid');
-        mid.innerHTML = `${config.saludo}, ${perfiles[0].nombre}`;
-
-        const searchBar = document.querySelector('li form');
-        searchBar.querySelector('input').setAttribute('placeholder', `${config.nombre}...`);
-        searchBar.querySelector('button').innerHTML = `${config.buscar}`;
+        mid.innerHTML = `${config.saludo}, ${data.perfiles[0].nombre}`;
 
         const footer = document.querySelector('footer');
         footer.innerHTML = `${config.copyRight}`;
+    
 
-    }
-        
-    document.body.appendChild(configLan);
+        function renderProfiles(perfiles) {
+            const gallery = document.querySelector('#gallery');
 
-    function renderProfiles(perfiles) {
-        const gallery = document.querySelector('#gallery');
+            perfiles.forEach(perfil => {
+                const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
+                const item = galleryItem.querySelector('#item');
+                item.onclick = ciCookie;
+                item.setAttribute('href', `index.py?ci=${perfil.ci}&lang=${language}`)
 
-        perfiles.forEach(perfil => {
-            const galleryItem = document.querySelector('#galleryItem').content.cloneNode(true);
-            galleryItem.querySelector('#item').setAttribute('href', `perfil.html?ci=${perfil.ci}&lang=${language}`)
-            galleryItem.querySelector('#galleryimg').setAttribute('src', `${perfil.imagen}`);
-            galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
-            gallery.appendChild(galleryItem);
+
+                function ciCookie() {
+                    document.cookie=`ci=${perfil.ci}`;
+                    window.location.reload();
+                    
+                    return false;
+                }
+                
+                galleryItem.querySelector('#galleryimg').setAttribute('src', `${perfil.imagen}`);
+                galleryItem.querySelector('label').innerHTML = `${perfil.nombre}`;
+                gallery.appendChild(galleryItem);
+            });
+        };
+
+        renderProfiles(data.perfiles);
+
+        const searchBar = document.getElementById('searchBar');
+        const searchButton = document.getElementById('searchButton');
+
+        searchButton.addEventListener('click', function(event) {
+            const query = searchBar.value.toLowerCase();
+            const results = data.perfiles.filter(perfil => perfil.nombre.toLowerCase().includes(query));
+
+            const section = document.querySelector('section');
+            section.innerHTML = `<ul id = "gallery"></ul>`;
+
+            if (query == "") {
+                renderProfiles(data.perfiles);
+            }else if (results.length > 0) {
+                renderProfiles(results);
+            } else {
+                const p = document.createElement('p');
+                p.id = "no_result";
+                p.innerHTML = config.no_resultado.replace('[query]', query);
+                document.querySelector('section').appendChild(p);
+            }
+            
+            event.preventDefault();
         });
-    };
-
-    renderProfiles(perfiles);
-
-    const searchBar = document.getElementById('searchBar');
-    const searchButton = document.getElementById('searchButton');
-
-    searchButton.addEventListener('click', function(event) {
-        const query = searchBar.value.toLowerCase();
-        const results = perfiles.filter(perfil => perfil.nombre.toLowerCase().includes(query));
-
-        const section = document.querySelector('section');
-        section.innerHTML = `<ul id = "gallery"></ul>`;
-
-        if (query == "") {
-            renderProfiles(perfiles);
-        }else if (results.length > 0) {
-            renderProfiles(results);
-        } else {
-            const p = document.createElement('p');
-            p.id = "no_result";
-            p.innerHTML = config.no_resultado.replace('[query]', query);
-            document.querySelector('section').appendChild(p);
-        }
-        
-        event.preventDefault();
     });
-
     
-    
-};
+});
--- js/perfil.js
+++ js/perfil.js
@@ -1,6 +1,17 @@
-window.onload = function () {
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     const params = new URLSearchParams(this.location.search)
-    const ci = params.get("ci");
+    const ci = getCookie("ci");
     const language = params.get("lang");
 
     if (!ci) {
@@ -9,55 +20,59 @@ window.onload = function () {
         return;
     }
 
-    const configLan = document.createElement('script');
-    configLan.src = `conf/config${language}.json`;
-    configLan.onload = function () {
+    const item = document.querySelector('#return');
+    item.onclick = ciCookie;
 
-        const script = document.createElement('script');
-        script.src = `${ci}/perfil.json`;
-        script.onload = function () {
-            if (typeof perfil !== "object") {
-                document.body.innerHTML = "<h2>El perfil no se cargó correctamente.</h2>";
-                return;
-            }
+    function ciCookie() {
+        document.cookie = 'ci=; Path=/ATI; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        console.log("deleted cookie");
+        window.location.reload();
+                        
+        return false;
+    }
 
-            document.title = perfil.nombre;
-            const profile = document.querySelector('.profile');
-            const picture = profile.querySelector("picture");
+    const config = fetch(`conf/config${language}.json`).then(response => response.json());
+    const perfil = fetch(`${ci}/perfil.json`).then(response => response.json());
 
-            picture.innerHTML = `<source media="(min-width:769px)" srcset="${ci}\/${perfil.ci}Grande.jpg"/>
-                                                            <source media="(min-width:320px)" srcset="${ci}\/${perfil.ci}Pequena.jpg"/>
-                                                            <img src="${ci}\/${perfil.ci}.jpg"/>`;
-            picture.querySelector('img').onerror = function () {
-                console.log("Fallo al cargar imagen: ", this);
-                perfiles.forEach(perfil => { 
+    Promise.all([config, perfil])
+    .then(([config, perfil]) => {
+        if (typeof perfil !== "object") {
+            document.body.innerHTML = "<h2>El perfil no se cargó correctamente.</h2>";
+            return;
+        }
+
+        document.title = perfil.nombre;
+        const profile = document.querySelector('.profile');
+        const picture = profile.querySelector("picture");
+
+        picture.innerHTML = `<source media="(min-width:769px)" srcset="${ci}\/${perfil.ci}Grande.jpg"/>
+                                                        <source media="(min-width:320px)" srcset="${ci}\/${perfil.ci}Pequena.jpg"/>
+                                                        <img src="${ci}\/${perfil.ci}.jpg"/>`;
+        picture.querySelector('img').onerror = function () {
+            console.log("Fallo al cargar imagen: ", this);
+            const data = fetch('datos/index.json').then(response => response.json());
+            Promise.all([data]).then(([data]) => {
+                data.perfiles.forEach(perfil => { 
                     if (perfil.ci == ci) {
                         picture.innerHTML = `<img src="${perfil.imagen}"/>`;
                     }
                 });
-            };
-
-            profile.querySelector('div h2').innerHTML = perfil.nombre;
-            profile.querySelector('#description'). innerHTML = perfil.descripcion;
-            const table = document.querySelector('table');
-            table.innerHTML = `<tr><td>${config.color}:</td> <td>${perfil.color}</td></tr>
-                            <tr><td>${config.libro}:</td> <td>${perfil.libro}</td></tr>
-                            <tr><td>${config.musica}:</td> <td>${perfil.musica.map(l => `${l}`).join(', ')}</td></tr>
-                            <tr><td>${config.video_juego}:</td> <td>${perfil.video_juego.map(l => `${l}`).join(', ')}</td></tr>
-                            <tr><td> <b>${config.lenguajes}:</b> </td> <td><b>${perfil.lenguajes.map(l => `${l}`).join(', ')}</b></td></tr>`;
-            
-            const email = document.querySelector('#email');
-            const emailText = config.email.replace("[email]", `<a id="email" href="mailto:${perfil.email}">${perfil.email}</a>`);
-            email.innerHTML = emailText;
+            });
         };
 
-        script.onerror = function () {
-            document.body.innerHTML = "<h2>Ups, no podemos encontrar datos de perfil.</h2>";
-            console.log("Directorio o archivo faltante en la ruta:", this.getAttribute('src'));
-        };
-
-        document.body.appendChild(script);
-    }
+        profile.querySelector('div h2').innerHTML = perfil.nombre;
+        profile.querySelector('#description'). innerHTML = perfil.descripcion;
+        const table = document.querySelector('table');
+        table.innerHTML = `<tr><td>${config.color}:</td> <td>${perfil.color}</td></tr>
+                        <tr><td>${config.libro}:</td> <td>${perfil.libro}</td></tr>
+                        <tr><td>${config.musica}:</td> <td>${perfil.musica.map(l => `${l}`).join(', ')}</td></tr>
+                        <tr><td>${config.video_juego}:</td> <td>${perfil.video_juego.map(l => `${l}`).join(', ')}</td></tr>
+                        <tr><td> <b>${config.lenguajes}:</b> </td> <td><b>${perfil.lenguajes.map(l => `${l}`).join(', ')}</b></td></tr>`;
+        
+        const email = document.querySelector('#email');
+        const emailText = config.email.replace("[email]", `<a id="email" href="mailto:${perfil.email}">${perfil.email}</a>`);
+        email.innerHTML = emailText;
+        
+    });
     
-    document.body.appendChild(configLan);
-};
+});
